25939394383cef2a63a93c05a74abb5d0ea24e0f
Radik Abubjazov <abubjazov@mail.ru>
2021-10-15 20:32:58 +0300

    секция preview: рефакторинг

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,6 @@
-import { BALANCED, FITNESS, LENTEN, PREMIUM } from "./types"
+import { tabHeaderHandler } from "./tabHeaderHandler"
 
-document.addEventListener('DOMContentLoaded', () => {
-    const tabHeaders = document.querySelectorAll('.tabheader__item'),
-          tabContent = document.querySelector('.tabcontent')
+document.addEventListener('DOMContentLoaded', () => {    
 
-    document.querySelector('.tabheader__items').addEventListener('click', event => {
-        event.preventDefault()
-
-        tabHeaders.forEach(tabheader => {
-            tabheader.classList.remove('tabheader__item_active')
-        })
-
-        event.target.classList.add('tabheader__item_active')
-
-        switch (event.target.dataset.item) {
-            case 'fitness':
-                tabContent.innerHTML = ''
-                tabContent.innerHTML = FITNESS
-                break;
-
-            case 'premium':
-                tabContent.innerHTML = ''
-                tabContent.innerHTML = PREMIUM
-                break;
-
-            case 'lenten':
-                tabContent.innerHTML = ''
-                tabContent.innerHTML = LENTEN
-                break;
-
-            case 'balanced':
-                tabContent.innerHTML = ''
-                tabContent.innerHTML = BALANCED
-                break;
-
-            default:
-                break;
-        }
-    })    
+    document.querySelector('.tabheader__items').addEventListener('click', tabHeaderHandler)
 })
---
+++ src/js/tabHeaderHandler.js
@@ -0,0 +1,39 @@
+import { BALANCED, FITNESS, LENTEN, PREMIUM } from "./types"
+
+export const tabHeaderHandler = event => {
+    event.preventDefault()
+
+    const tabHeaders = document.querySelectorAll('.tabheader__item'),
+          tabContent = document.querySelector('.tabcontent')
+
+    tabHeaders.forEach(tabheader => {
+        tabheader.classList.remove('tabheader__item_active')
+    })
+
+    event.target.classList.add('tabheader__item_active')
+
+    switch (event.target.dataset.item) {
+        case 'fitness':
+            tabContent.innerHTML = ''
+            tabContent.innerHTML = FITNESS
+            break;
+
+        case 'premium':
+            tabContent.innerHTML = ''
+            tabContent.innerHTML = PREMIUM
+            break;
+
+        case 'lenten':
+            tabContent.innerHTML = ''
+            tabContent.innerHTML = LENTEN
+            break;
+
+        case 'balanced':
+            tabContent.innerHTML = ''
+            tabContent.innerHTML = BALANCED
+            break;
+
+        default:
+            break;
+    }
+} 
