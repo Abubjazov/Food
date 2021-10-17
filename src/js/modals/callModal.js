0d83c733ed8d4b164e683bd80575f480ff2d04f0
Radik Abubjazov <abubjazov@mail.ru>
2021-10-17 21:24:37 +0300

    progressBar: + вызов modal по событию scroll в конце страницы

--- src/js/modals/callModal.js
+++ src/js/modals/callModal.js
@@ -1,14 +1,10 @@
-export const callModal = (e) => {
-    e.preventDefault()
-
+export const callModal = () => {
     const modal = document.querySelector('.modal')
 
     if (modal.style.display === 'none') {
         modal.style.display = ''
 
         modal.addEventListener('click', e => {
-            e.preventDefault()
-
             if (e.target === modal) {
                 modal.style.display = 'none'
             }
--- src/js/progressBars/progressBar.js
+++ src/js/progressBars/progressBar.js
@@ -1,5 +1,11 @@
+import { callModal } from "../modals/callModal"
+
 export const progressBar = () => {
     const progressBar = document.querySelector('.progressbar')
 
     progressBar.style.width = (document.scrollingElement.scrollTop * 100) / (document.scrollingElement.scrollHeight - document.scrollingElement.clientHeight) + '%'
+
+    if (parseInt(progressBar.style.width) === 100) {
+        setTimeout(callModal, 2000)
+    }
 }
