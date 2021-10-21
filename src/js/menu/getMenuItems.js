72ac53c3f1731c3d5d736b90433f967e6fc28c4e
Radik Abubjazov <abubjazov@mail.ru>
2021-10-21 22:41:52 +0300

    getMenuItems: использование axios вместо fetch

--- src/js/menu/getMenuItems.js
+++ src/js/menu/getMenuItems.js
@@ -1,4 +1,7 @@
 export const getMenuItems = async () => {
-    return await fetch('http://localhost:3000/menu')
-                .then(data => data.json())
+    // return await fetch('http://localhost:3000/menu')
+    //             .then(data => data.json())
+
+    return await axios.get('http://localhost:3000/menu')
+                .then(data => data.data)
 }
